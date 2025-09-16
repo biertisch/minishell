3d639393ccd3de9c6d085afba4e1902b64c114cd
Pedro Lourenço <pedrolourencoemail@gmail.com>
2025-09-16 15:23:50 +0100

    no more bad closes

--- src/child.c
+++ src/child.c
@@ -37,7 +37,6 @@ void	child_redir_in(t_data *data, t_stack **stack)
 	close(((*stack)->node->redir)->fd);
 	close((*stack)->out_fd);
 	close((*get_first_pipe(stack))->pipe[0]);
-	close((*get_first_pipe(stack))->pipe[1]);
 	check_for_errors(execve((*stack)->node->argv[0], (*stack)->node->argv, data->env), data, *stack, "execve");
 }
 
@@ -60,8 +59,6 @@ void	child_redir_out(t_data *data, t_stack **stack)
 	check_for_errors(dup2((*stack)->in_fd, STDIN_FILENO), data, *stack, "dup2");
 	close(((*stack)->node->redir)->fd);
 	close((*stack)->in_fd);
-	close((*stack)->old_fd);
-	close((*get_first_pipe(stack))->pipe[0]);
 	close((*get_first_pipe(stack))->pipe[1]);
 	check_for_errors(execve((*stack)->node->argv[0], (*stack)->node->argv, data->env), data, *stack, "execve");
 }
